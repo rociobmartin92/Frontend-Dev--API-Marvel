2021cd0e51533971fcd451517dd5ff0ec3104ea1
rociobmartin92 <77502194+rociobmartin92@users.noreply.github.com>
2023-11-30 13:18:30 -0300

    feature: added function to pick random superheroe

--- src/views/Play.jsx
+++ src/views/Play.jsx
@@ -4,36 +4,81 @@ const Play = (props) => {
   const allCharacters = props.characters;
 
   const [firsPlayer, setFirsPlayer] = useState({});
-  const [setsecondPlayer, setSetsecondPlayer] = useState({});
+  const [secondPlayer, setSecondPlayer] = useState({});
+  const [show, setShow] = useState(false);
+  const [rounds, setRounds] = useState(0);
 
-  //   const getRandomCharacter = () => {
-  //     const indexOne = Math.floor(Math.random() * allCharacters.length);
-  //     let indexTwo;
+  const getRandomCharacter = async () => {
+    const indexOne = Math.floor(Math.random() * allCharacters.length);
+    let indexTwo;
 
-  //     do {
-  //       indexTwo = Math.floor(Math.random() * allCharacters.length);
-  //     } while (indexOne === indexTwo);
+    do {
+      indexTwo = await Math.floor(Math.random() * allCharacters.length);
+    } while (indexOne === indexTwo);
 
-  //     setFirsPlayer(allCharacters[indexOne]);
-  //     setSetsecondPlayer(allCharacters[indexTwo]);
-  //   };
+    setFirsPlayer(allCharacters[indexOne]);
+    setSecondPlayer(allCharacters[indexTwo]);
 
-  console.log("All in PLAY", allCharacters);
+    setShow(true);
+  };
+
+  //   console.log("First in PLAY", firsPlayer);
+  //   console.log("Second in PLAY", secondPlayer);
+  function handleChange(event) {
+    console.log(event.target.value);
+    setRounds(event.target.value);
+  }
+
+  console.log(rounds, "ROUNDS");
 
   return (
     <>
       {/* {loading && <p> BUSCANDO PERSONAJES..</p>} */}
-      <p>Play</p>
-      <div>
-        <div>
-          <img />
-          <p>Jugador 1 </p>
-        </div>
-        <div>
-          <img />
-          <p>Jugador </p>
-        </div>
+      <div className="">
+        <label>Ingresa la cantidad de rounds:</label> <br /> <br />
+        <input
+          min={0}
+          onChange={handleChange}
+          type="number"
+          required
+          size="10"
+          placeholder="0"
+          className="w-[7%] pl-2"
+        />
       </div>
+      <button
+        disabled={rounds > 0 ? false : true}
+        className={
+          rounds > 0
+            ? "bg-red-600  text-white p-2 rounded-lg mt-4"
+            : "bg-green-700  text-white p-2 rounded-lg mt-4"
+        }
+        onClick={() => getRandomCharacter()}
+      >
+        A JUGAR
+      </button>
+      {show && (
+        <div className="sm:flex justify-center mt-7 ">
+          <div className="flex-col mr-3">
+            <img
+              alt={firsPlayer.name}
+              src={`${firsPlayer.thumbnail.path}.jpg`}
+              className="h-[200px] w-[200px] rounded-md "
+            />
+            <p>Jugador 1 </p>
+            <p>{firsPlayer.name}</p>
+          </div>
+          <div className="flex-col ml-3">
+            <img
+              alt={secondPlayer.name}
+              src={`${secondPlayer.thumbnail.path}.jpg`}
+              className="h-[200px] w-[200px] rounded-md "
+            />
+            <p>Jugador 2</p>
+            <p>{secondPlayer.name}</p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
