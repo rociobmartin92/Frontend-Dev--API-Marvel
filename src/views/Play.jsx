e8f7960b215d7ce675fe872d092c865b024d1a3e
rociobmartin92 <77502194+rociobmartin92@users.noreply.github.com>
2023-12-01 14:31:29 -0300

    feature: added winners logic

--- src/views/Play.jsx
+++ src/views/Play.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ref, onValue, set } from "firebase/database";
 import { db } from "../firebase/conection";
 import { useGetFights } from "../hooks/useGetFights";
@@ -6,38 +6,64 @@ import { useGetFights } from "../hooks/useGetFights";
 const Play = (props) => {
   const allCharacters = props.characters;
   const [rounds, setRounds] = useState(0);
+  const [winners, setWinners] = useState([]);
 
   const { getFights, fights } = useGetFights(allCharacters, rounds);
 
-  // const [firsPlayer, setFirsPlayer] = useState({});
-  // const [secondPlayer, setSecondPlayer] = useState({});
-  // const [indexOne, setIndexOne] = useState(0);
-  // const [indexTwo, setIndexTwo] = useState(0);
-  // const [show, setShow] = useState(false);
-  // const [winner, setWinner] = useState({});
+  const [show, setShow] = useState(false);
 
   const handleChange = (event) => {
     // console.log(typeof Number(event.target.value));
     setRounds(Number(event.target.value));
   };
 
-  // WRITE IN DB
+  const determineWinners = () => {
+    const newWinners = [];
 
-  const saveWinner = (characterId) => {
-    set(ref(db, "winners/" + characterId), {
-      // body
+    fights.forEach((pair) => {
+      const winnerInPair = pair.reduce((maxID, character) => {
+        return character.id > maxID ? character.id : maxID;
+      }, -1);
+
+      const winnerCharacter = pair.find(
+        (character) => character.id === winnerInPair
+      );
+      newWinners.push(winnerCharacter);
     });
+
+    setWinners(newWinners);
   };
 
-  // console.log(rounds, "ROUNDS");
-  // console.log(winner, "WINNER");
+  const onHandleSetWinners = () => {
+    // determineWinners();
+    setShow(true);
+    saveWinner();
+    const saveWinner = (winners) => {
+      set(ref(db, "winners/" + winners), {
+        // body
+      });
+    };
+  };
+
+  useEffect(() => {
+    determineWinners();
+  }, [fights]);
+
+  // // WRITE IN DB
+  // const saveWinner = (characterId) => {
+  //   set(ref(db, "winners/" + characterId), {
+  //     // body
+  //   });
+  // };
+
   console.log(fights, "Fights");
+  console.log(winners, "WINNERS");
 
   return (
     <div className="text-center">
       {/* {loading && <p> BUSCANDO PERSONAJES..</p>} */}
       <div className="">
-        <label>Ingresa la cantidad de rounds:</label> <br /> <br />
+        <label>Ingresa el número de veladas:</label> <br /> <br />
         <input
           min={0}
           onChange={handleChange}
@@ -60,7 +86,61 @@ const Play = (props) => {
         A JUGAR
       </button>
 
-      {/* {show && ( */}
+      {!show && (
+        <div className="mt-5">
+          {fights.map((pair, index) => (
+            <>
+              {/* <p
+                className=" text-lg text-red-600  font-bold mt-6"
+                style={{ marginBottom: "-55px" }}
+              >
+                vs
+              </p> */}
+              <div
+                key={index}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  marginBottom: "20px",
+                }}
+              >
+                {pair.map((character) => (
+                  <div
+                    key={character.id}
+                    className="sm:flex justify-center mt-7 "
+                  >
+                    <div className="sm:flex-col mr-3 text-center">
+                      <h2 className="text-red-600 mb-3 italic mt-1">
+                        {character.name}
+                      </h2>
+                      <img
+                        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                        alt={character.name}
+                        className="h-[280px] w-[280px] rounded-md "
+                      />
+                      <p className="font-semibold mt-3 text-lg">
+                        Poder: {character.id}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          ))}
+          {winners.map((winner, index) => (
+            <div key={winner.id} className="flex">
+              <p>Ganador Velada N:{index + 1} </p>
+              <p className="ml-2"> {winner.name} </p>
+            </div>
+          ))}
+          <button onClick={() => onHandleSetWinners()}>
+            Ver historial de ganadores
+          </button>
+        </div>
+      )}
+
+      {show && winners.map((el) => <p key={el.id}> {el.name} </p>)}
+      {/* {show && ( 
       {/* <div>
         <div className="sm:flex justify-center mt-7 ">
           <div className="sm:flex-col mr-3 text-center">
@@ -84,7 +164,7 @@ const Play = (props) => {
             <p className="font-semibold text-lg">Poder: {indexTwo}</p>
           </div>
         </div>
-        <p className="font-semibold text-lg mt-5">GANADOR: </p>
+
       </div> */}
       {/* )} */}
     </div>
