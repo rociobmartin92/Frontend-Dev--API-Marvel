138c8bc3001fe1c273ffc275b84dc90c53140099
rociobmartin92 <77502194+rociobmartin92@users.noreply.github.com>
2023-12-04 14:38:21 -0300

    bugfix: pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,14 +7,14 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   }
   console.log(postsPerPage, totalPosts, paginate);
   return (
-    <nav className="cursor-pointer">
+    <nav className="cursor-pointer absolute bottom-0 pb-6 ml-5">
       <ul className="flex">
         {pageNumbers.map((number) => (
           <li
             key={number}
-            className="mx-2 border-red-800 border-2 px-2  py-1 rounded-md"
+            className="mx-2 border-red-800 border-2 px-3  py-1 rounded-md hover:bg-gray-300 hover:text-red-800 hover:font-bold"
           >
-            <a onClick={() => paginate(number)} href="!#" className="">
+            <a onClick={() => paginate(number)} className="">
               {number}
             </a>
           </li>
